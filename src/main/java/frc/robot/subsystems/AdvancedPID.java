356349a1818dd1c3fe1912d951d5a6baac3cf789
Junbo Wang <jbwang2026@gmail.com>
2024-12-13 14:09:28 -0500

    elevator code

--- src/main/java/frc/robot/subsystems/AdvancedPID.java
+++ src/main/java/frc/robot/subsystems/AdvancedPID.java
@@ -7,16 +7,20 @@ import com.ctre.phoenix6.configs.TalonFXConfiguration;
 import com.ctre.phoenix6.controls.MotionMagicVoltage;
 import com.ctre.phoenix6.controls.PositionVoltage;
 import com.ctre.phoenix6.hardware.TalonFX;
+import com.ctre.phoenix6.signals.GravityTypeValue;
 import com.ctre.phoenix6.signals.InvertedValue;
 import com.ctre.phoenix6.signals.NeutralModeValue;
+import com.ctre.phoenix6.signals.StaticFeedforwardSignValue;
 
+import edu.wpi.first.math.trajectory.TrapezoidProfile;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class AdvancedPID extends SubsystemBase{ // EXTENDS SUBSYSTEMBASE!!!!!!!!!
     private TalonFX myTalon;
-    final PositionVoltage m_request = new PositionVoltage(0).withSlot(0);
+    private PositionVoltage m_request;
+    private TrapezoidProfile m_profile;
 
     public AdvancedPID() {
         myTalon = new TalonFX(50);
@@ -36,17 +40,23 @@ public class AdvancedPID extends SubsystemBase{ // EXTENDS SUBSYSTEMBASE!!!!!!!!
         // PDSAV Settings
         // Mainly for overshoot + undershoot tuning
         var slot0Configs = new Slot0Configs();
+        // 0.5V is neede
+        slot0Configs.kG = 0.20; // Gravity 0.2
+        slot0Configs.GravityType = GravityTypeValue.Elevator_Static;
+        slot0Configs.kS = 0.30; // Friction 0.3
+        slot0Configs.StaticFeedforwardSign = StaticFeedforwardSignValue.UseClosedLoopSign;
+        slot0Configs.kV = 1; // Velocity
+        slot0Configs.kA = 0; // Acceleration
         slot0Configs.kP = 0; // Proportion
         slot0Configs.kD = 0; // Derivative
-        slot0Configs.kS = -1; // Friction
-        slot0Configs.kV = 0; // Velocity
-        slot0Configs.kA = 0; // Acceleration
 
+        m_profile= new TrapezoidProfile(
+            new TrapezoidProfile.Constraints(15, 30));
         // Motion Magic (Trapezoid speed control)
         //var motionMagicConfigs = talonFXConfigs.MotionMagic;
-        //talonFXConfigs.MotionMagic.MotionMagicCruiseVelocity = 10; 
-        //talonFXConfigs.MotionMagic.MotionMagicAcceleration = 3; 
-        //motionMagicConfigs.MotionMagicJerk = 0; 
+        //talonFXConfigs.MotionMagic.MotionMagicCruiseVelocity = 10; // 10 rot/sec
+        //talonFXConfigs.MotionMagic.MotionMagicAcceleration = 20; // 0.5 sec to reach cruise v
+        //motionMagicConfigs.MotionMagicJerk = 0;
 
         var motorConfigs = new MotorOutputConfigs();
 
@@ -55,8 +65,11 @@ public class AdvancedPID extends SubsystemBase{ // EXTENDS SUBSYSTEMBASE!!!!!!!!
         myTalon.getConfigurator().apply(motorConfigs);
         myTalon.getConfigurator().apply(talonFXConfigs);
         myTalon.getConfigurator().apply(slot0Configs);
+        //myTalon.getConfigurator().apply(motionMagicConfigs);
 
         myTalon.setPosition(0);
+
+        m_request= new PositionVoltage(0).withSlot(0);
     }
 
     // Setting elevator talon to spin to a certain height
@@ -64,14 +77,22 @@ public class AdvancedPID extends SubsystemBase{ // EXTENDS SUBSYSTEMBASE!!!!!!!!
     public Command talonSet(double setpoint) {
         return runOnce(() -> 
         {
-            myTalon.setControl(m_request.withPosition(setpoint));
+            TrapezoidProfile.State m_goal = new TrapezoidProfile.State(setpoint, 0);
+            TrapezoidProfile.State m_setpoint = new TrapezoidProfile.State();
+            m_setpoint = m_profile.calculate(3, m_setpoint, m_goal);
+            m_request.Position = m_setpoint.position;
+            m_request.Velocity = m_setpoint.velocity;
+            myTalon.setControl(m_request);
         });
     }
 
     @Override
     public void periodic() {
-        SmartDashboard.putNumber("Motor Rotations", myTalon.getPosition().getValueAsDouble());
+        SmartDashboard.putNumber("Motor Rotations", myTalon.getPosition(true).getValueAsDouble());
         SmartDashboard.putNumber("Closed Loop Output", myTalon.getClosedLoopOutput().getValueAsDouble());
+        SmartDashboard.putNumber("FF Output", myTalon.getClosedLoopFeedForward().getValueAsDouble());
+        SmartDashboard.putNumber("Velocity", myTalon.getClosedLoopFeedForward().getValueAsDouble());
+        
     }
 }
 // Motion magic sysID
